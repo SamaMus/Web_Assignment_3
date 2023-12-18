3f2ad1490392fdaf92af1e3f178a9eb3e00b422d
Sama <mustafalinihad8@gmail.com>
2023-12-18 19:49:10 +0400

    style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css'; // Import the CSS file
 
 const projects = [
   { id: 1, title: 'Project 1', description: 'Description for Project 1' },
@@ -9,13 +10,15 @@ const projects = [
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Welcome to My Portfolio</h1>
       {projects.map((project) => (
-        <div key={project.id}>
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
-          <Link to={`/projects/${project.id}`}>Read more</Link>
+        <div key={project.id} className="project-box">
+          <h3 className="project-title">{project.title}</h3>
+          <p className="project-description">{project.description}</p>
+          <Link to={`/projects/${project.id}`} className="read-more-link">
+            Read more
+          </Link>
         </div>
       ))}
     </div>
