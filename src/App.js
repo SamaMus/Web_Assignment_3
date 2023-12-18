74de52c437eaeb5dea235d6122fddcc5390ebd86
Sama <mustafalinihad8@gmail.com>
2023-12-18 19:45:04 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import ProjectPage from './pages/ProjectPage';
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/projects/:projectId" component={ProjectPage} />
       </Switch>
     </Router>
   );
