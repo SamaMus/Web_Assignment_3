e390593c821cc6c744da3eb3f74b88b30c96680c
Sama <mustafalinihad8@gmail.com>
2023-12-31 00:34:26 +0400

    flashcard

---
+++ src/pages/FlashCard.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import './FlashCard.css';
+
+const FlashCard = ({
+  card,
+  onEdit,
+  onDelete,
+  onSelect,
+  isSelected,
+  onRearrange,
+}) => {
+  const { id, frontText, backAnswer, image, lastModificationDateTime, status } = card;
+  const [isFlipped, setFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setFlipped(!isFlipped);
+  };
+
+  const handleStatusChange = (newStatus) => {
+    // You can implement the status change logic here if needed
+  };
+
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData('text/plain', id);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const draggedCardId = e.dataTransfer.getData('text/plain');
+    onRearrange(draggedCardId, id);
+  };
+
+  return (
+    <div
+      className={`flash-card ${isFlipped ? 'flipped' : ''} ${isSelected ? 'selected' : ''}`}
+      onMouseEnter={onEdit}
+      onMouseLeave={onDelete}
+      onClick={() => onSelect(id)}
+      draggable
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+      <div className="card-content front">
+        <div className="content">
+          <h3>{frontText}</h3>
+          <p>Status: {status}</p>
+          <p>Last Modified: {new Date(lastModificationDateTime).toLocaleString()}</p>
+        </div>
+      </div>
+      <div className="card-content back">
+        <div className="content">
+          <img src={image} alt="Card Back" />
+          <h3>{backAnswer}</h3>
+          <p>Status: {status}</p>
+          <p>Last Modified: {new Date(lastModificationDateTime).toLocaleString()}</p>
+          <div className="status-buttons">
+            <button onClick={() => handleStatusChange('Learned')}>Learned</button>
+            <button onClick={() => handleStatusChange('Want to Learn')}>Want to Learn</button>
+            <button onClick={() => handleStatusChange('Noted')}>Noted</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlashCard;
