44ebe46ad052ff55455ec3a81cc27ca14afadfa4
Sama <mustafalinihad8@gmail.com>
2023-12-31 00:20:06 +0400

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Home.css';
-import { Link } from 'react-router-dom'; 
 
 const Home = () => {
+  // State to store the projects data
+  const [projects, setProjects] = useState([]);
+
+  // useEffect hook to fetch projects when the component mounts
+  useEffect(() => {
+    // Function to fetch projects from the server
+    const fetchProjects = async () => {
+      try {
+        // Make a GET request to the server endpoint
+        const response = await axios.get('http://localhost:3001/projects');
+        // Set the fetched projects in the state
+        setProjects(response.data);
+      } catch (error) {
+        // Log an error message if there's an issue fetching projects
+        console.error('Error fetching projects:', error);
+      }
+    };
+
+    // Call the fetchProjects function when the component mounts
+    fetchProjects();
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
   return (
     <div>
+      {/* Header section */}
       <header>
         <h1>Welcome to My Portfolio</h1>
+        {/* Navigation links */}
         <div className="header-buttons">
           <Link to="/flashcards">Flashcards</Link>
           <Link to="/contact">Contact Me</Link>
           <Link to="/">Home</Link>
         </div>
       </header>
+      {/* Main content section */}
       <div className="content">
-        <p>General information about yourself...</p>
+        <p>General information about yourself</p>
 
         <h2>Projects</h2>
+        {/* Project list */}
         <div className="project-list">
+          {/* Map through the projects and render each project */}
           {projects.map((project) => (
             <div key={project.id} className="project">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
+              {/* Link to view the project (opens in a new tab) */}
               <a href={project.link} target="_blank" rel="noopener noreferrer">
                 View Project
               </a>
