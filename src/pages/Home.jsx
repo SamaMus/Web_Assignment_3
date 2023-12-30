abffbe1a3b90d681a9dd7f6e8c89db00d5d8bab0
Sama <mustafalinihad8@gmail.com>
2023-12-30 23:43:06 +0400

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,6 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom'; 
-const projects = [
-  {
-    id: 1,
-    title: 'React Tasks',
-    description: 'A project showcasing various tasks implemented using React.',
-    link: 'https://example.com/react-tasks',
-  },
-  {
-    id: 2,
-    title: 'JavaScript Tasks',
-    description: 'A collection of tasks implemented using JavaScript.',
-    link: 'https://example.com/javascript-tasks',
-  },
-  {
-    id: 3,
-    title: 'Personal Website',
-    description: 'My personal website built with HTML, CSS, and JavaScript.',
-    link: 'https://example.com/personal-website',
-  },
-  {
-    id: 4,
-    title: 'Software Engineering Project',
-    description: 'A software engineering project focusing on XYZ.',
-    link: 'https://example.com/software-engineering-project',
-  },
-  {
-    id: 5,
-    title: 'Web Mobile Assignment 2',
-    description: 'Assignment 2 for a web and mobile development course.',
-    link: 'https://example.com/web-mobile-assignment2',
-  },
-];
-
-// ... (previous code)
 
 const Home = () => {
   return (
